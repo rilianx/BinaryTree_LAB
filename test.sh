4e56f713ab63194ef0eef6442425fffb384de779
rilianx <rilianx@gmail.com>
2020-09-18 23:30:39 -0300

    new try

--- test.sh
+++ test.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
-# clear
-# echo " Que funcionalidad desea testear?"
-# echo "1. Create"
-# echo "2. Insert"
-# echo "3. Search"
-# echo "4. Erase"
-# echo "5. First"
-# echo "6. Next"
-# echo "7. UpperBound"
-# echo -n "Seleccione una opcion [1 - 7]"
-# read opcion
-# case $opcion in
-# 1) ./a.out create;;
-# 2) ./a.out insert;;
-# 3) ./a.out search;;
-# 4) ./a.out erase;;
-# 5) ./a.out first;;
-# 6) ./a.out next;;
-# 7) ./a.out ub;;
-# *) echo "$opc es una opcion invalida?";;
-
-
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>log.out 2>&1
 # Everything below will go to the file 'log.out':
 
-echo "$(date): " &>1
+echo "$(date): "
 git diff --stat --ignore-space-at-eol -b -w --ignore-blank-lines treemap.c
 
 if gcc test.c -o a.out ; then
@@ -41,7 +19,26 @@ git push &> /dev/null
 
 
 
-
+clear
+echo " Que funcionalidad desea testear?"
+echo "1. Create"
+echo "2. Insert"
+echo "3. Search"
+echo "4. Erase"
+echo "5. First"
+echo "6. Next"
+echo "7. UpperBound"
+echo -n "Seleccione una opcion [1 - 7]"
+read opcion
+case $opcion in
+1) ./a.out create  &>1;;
+2) ./a.out insert;;
+3) ./a.out search;;
+4) ./a.out erase;;
+5) ./a.out first;;
+6) ./a.out next;;
+7) ./a.out ub;;
+*) echo "$opc es una opcion invalida?";;
 
 
 
