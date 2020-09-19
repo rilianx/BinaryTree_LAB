67d5be948e2a3391d208fedb9dc9177c9a79aad6
rilianx <rilianx@gmail.com>
2020-09-18 23:28:25 -0300

    new try

--- test.sh
+++ test.sh
@@ -26,7 +26,7 @@ trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>log.out 2>&1
 # Everything below will go to the file 'log.out':
 
-echo "$(date): " 
+echo "$(date): " &>1
 git diff --stat --ignore-space-at-eol -b -w --ignore-blank-lines treemap.c
 
 if gcc test.c -o a.out ; then
