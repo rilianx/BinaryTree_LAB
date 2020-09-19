5e4c9d5d4ab697514eee4af1cc0cf064df4e564b
rilianx <rilianx@gmail.com>
2020-09-18 22:54:40 -0300

    new try

--- treemap.c
+++ treemap.c
@@ -38,7 +38,7 @@ TreeNode * createTreeNode(void* key, void * value) {
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
     //new->lower_than = lower_than;
-    return NULL
+    return NULL;
 }
 
 
