3f92480e094fcaffdd46e4fa58db032b90e1cda2
Ignacio Daniel Araya Zamorano <rilianx@gmail.com>
2025-04-11 08:44:32 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,13 +4,13 @@
 
 # Everything below will go to the file 'log':
 
-target_file="treemap.c"
+target_file="list.c"
 testcode="test.c"
 
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
-#Verifca si se realizaron cambios 
+#Verifica si se realizaron cambios 
 CHANGED=$(git diff --ignore-space-at-eol -b -w --ignore-blank-lines $target_file)
 
 #Si hubieron cambios se actualiza el log
@@ -21,37 +21,42 @@ if [ -n "$CHANGED" ] || [ ! -f "a.out" ]; then
   echo "$(date): " >> log
   git diff --stat --ignore-space-at-eol -b -w --ignore-blank-lines $target_file >> log
 
+  if ! rm -f a.out 2>/dev/null; then
+      echo "No se pudo eliminar 'a.out'. Por favor elimínalo manualmente." >&3
+      exit 1
+  fi
+  echo "Eliminando 'a.out'..." >&3
+
   #Compilation
-  echo "Compiling with: gcc $testcode -Wall -Werror -o a.out" >&3
-  if gcc $testcode -Wall -Werror -o a.out 2>>log ; then
-      echo  " tests: " $(($(./a.out | grep -c 'OK')))\|$(($(./a.out | grep -c 'FAILED'))) >> log
+  echo "Compilando: gcc -g $testcode -Wall -Werror -o a.out" >&3
+  gcc -g $testcode -Wall -Werror -o a.out 2>gcc_err.log
+
+  if [ $? -eq 0 ]; then
+      echo " tests: " $(($(./a.out | grep -c 'OK')))\|$(($(./a.out | grep -c 'FAILED'))) >> log
       ./a.out | tail -n1 >> log
       git rev-parse --short HEAD >> log
-      #git add and commit
       git add $target_file log &> /dev/null 
       git commit -m 'new try' &> /dev/null 
   else
-      echo "Compilation with errors :c" >&3
+      echo "Complación con errores :c" >&3
       echo "Compilation failed" >> log
-      gcc $testcode -Wall -Werror -o a.out 2>&3
+      cat gcc_err.log >&3   # muestra los errores reales al usuario
+      cat gcc_err.log >> log
       git rev-parse --short HEAD >> log
-      #git add and commit
       git add $target_file log &> /dev/null 
       git commit -m 'new try' &> /dev/null 
       exit 1
   fi
 
- 
+
 fi
 
 #Retroalimentation
 
 if ./a.out &> /dev/null ; then
-  echo "Executing: ./a.out" >&3
+  echo "Ejecutando: ./a.out" >&3
   
   ./a.out >&3
-
-
 
   echo "Quiere actualizar su avance en el servidor? (s|n): " >&3
   read response
@@ -63,8 +68,17 @@ if ./a.out &> /dev/null ; then
   
 
 else
-  echo "Errors in execution =O" >&3
-  gcc -g $testcode -o a.out >&3
-  gdb -silent -ex='set disable-randomization off' -ex='set confirm off' -ex='run' -ex=quit a.out >&3
+  echo "Ejecución con errores =O" >&3
+
+  gdb -silent -ex='set disable-randomization off' -ex='set confirm off' \
+      -ex='set logging file '"tmpout" -ex='run' -ex='bt' -ex=quit a.out >&3
+
+  if grep -q "total_score" "tmpout"; then
+       echo; echo "Debugger no encontró errores, pero el programa falló:" >&3
+      ./a.out >&3
+  else
+      echo;echo "Programa falló" >&3
+      
+  fi 
 
 fi
